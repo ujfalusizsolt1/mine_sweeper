caab1478a032c20da0c437213580aa6f87701932
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-10-25 11:51:23 +0200

    játéktér kész

--- mine_sweeper.py
+++ mine_sweeper.py
@@ -1,10 +1,24 @@
 gameover = False
 
+import random
+
+table_list= []
+
+for i in range(0,9):
+    table_list.append([])
+    for j in range(0,9):
+        table_list[i].append(0)
+
+for i in range(0,15):
+    table_list[random.randint(0, 8)][random.randint(0, 8)] = 1
 
 
+for i in range(0, 9):
+    for j in range(0, 9):
+        print(table_list[i][j], end=' ')
+    print("")
 
-while gameover:
 
-while gameover = False:
+#while gameover = False:
     
 
