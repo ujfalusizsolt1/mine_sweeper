3254b5239b2b294c88773c7b8530f0249ee256be
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-10-25 13:23:37 +0200

    table+mine list

--- mine_sweeper.py
+++ mine_sweeper.py
@@ -4,18 +4,29 @@ import random
 
 table_list= []
 
+mine_list=[]
+
 for i in range(0,9):
     table_list.append([])
+    mine_list.append([])
     for j in range(0,9):
         table_list[i].append(0)
+        mine_list[i].append(0)
 
 for i in range(0,15):
-    table_list[random.randint(0, 8)][random.randint(0, 8)] = 1
+    mine_list[random.randint(0, 8)][random.randint(0, 8)] = 10
 
 
 for i in range(0, 9):
     for j in range(0, 9):
-        print(table_list[i][j], end=' ')
+        if table_list[i][j] == 0:
+            print("O", end=" ")
+        elif table_list[i][j] == 10:
+            print("#", end=" ")
+        elif table_list[i][j] == 9:
+            print("", end=" ")
+        else:
+            print(table_list[i][j], end=' ')
     print("")
 
 
