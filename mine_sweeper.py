5d54e7c8d37cf665ecaed6bf2345cbfe9686f519
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-10-25 14:23:41 +0200

    check

--- mine_sweeper.py
+++ mine_sweeper.py
@@ -29,30 +29,89 @@ for i in range(0, 9):
             print(table_list[i][j], end=' ')
     print("")
 
+    minecounter = 15
 
-while gameover = False:
+while gameover == False:
+
+    
+    check=bool(input("Do you want to check the the next target? "))
     firstcoord=int(input("Please give the number of the row you want to check: "))
     secondcoord=int(input("Please give the number of the column you want to check: "))
 
+
     if mine_list[firstcoord][secondcoord] == 10:
-        gameover = True
+        if check == True:
+            minecounter -= 1
+        elif check == False:
+            gameover = True
+            print("Aknára léptél és felrobbantál a picsába.")
 
     mines = 0
 
-    if gameover == False:
+    if gameover == False and check == False:
         if firstcoord == 0:
             for i in range(0,1):
                 if secondcoord == 0:
                     for j in range(0,1):
+                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                            mines += 1
                 elif secondcoord == 8:
                     for j in range(-1,0):
+                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                            mines += 1
                 else:
                     for j in range(-1,1):
+                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                            mines += 1
         elif firstcoord == 8:
             for i in range(-1,0):
+                if secondcoord == 0:
+                    for j in range(0,1):
+                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                            mines += 1
+                elif secondcoord == 8:
+                    for j in range(-1,0):
+                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                            mines += 1
+                else:
+                    for j in range(-1,1):
+                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                            mines += 1
         else:
             for i in range(-1,1):
+                if secondcoord == 0:
+                    for j in range(0,1):
+                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                            mines += 1
+                elif secondcoord == 8:
+                    for j in range(-1,0):
+                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                            mines += 1
+                else:
+                    for j in range(-1,1):
+                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                            mines += 1
+        table_list[firstcoord][secondcoord] = mines
+        mines = 0
 
+
+    for i in range(0, 9):
+            for j in range(0, 9):
+                if table_list[i][j] == 0:
+                    print("O", end=" ")
+                elif table_list[i][j] == 10:
+                    print("#", end=" ")
+                elif table_list[i][j] == 9:
+                    print("", end=" ")
+                else:
+                    print(table_list[i][j], end=' ')
+            print("")
+
+
+    for i in range(0,9):
+        for j in range(0,9):
+            print(mine_list[i][j], end= ' ')
+        print("")
 
 
         
