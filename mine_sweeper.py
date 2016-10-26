ac5aebe6a1a410ba44906b7dd0efdb0b6fd293e6
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-10-26 11:32:25 +0200

    s git teljesen hulye

--- mine_sweeper.py
+++ mine_sweeper.py
@@ -6,6 +6,7 @@ table_list= []
 
 mine_list=[]
 
+# 0-val táblázat feltöltés
 for i in range(0,9):
     table_list.append([])
     mine_list.append([])
@@ -13,107 +14,117 @@ for i in range(0,9):
         table_list[i].append(0)
         mine_list[i].append(0)
 
+# aknák elhelyezése
 for i in range(0,15):
-    mine_list[random.randint(0, 8)][random.randint(0, 8)] = 10
+    table_list[random.randint(0, 8)][random.randint(0, 8)] = 10
+
+for i in range(0,9):
+    for j in range(0,9):
+        if table_list[i][j] == 10:
+            mine_list[i][j] = "X"
 
 
+# table felprintelése
 for i in range(0, 9):
     for j in range(0, 9):
-        if table_list[i][j] == 0:
-            print("O", end=" ")
-        elif table_list[i][j] == 10:
-            print("#", end=" ")
-        elif table_list[i][j] == 9:
-            print("", end=" ")
-        else:
-            print(table_list[i][j], end=' ')
+        print("O", end=" ")
     print("")
 
-    minecounter = 15
+    minecounter = 14
 
 while gameover == False:
 
     
-    mark=bool(input("Do you want to mark the the next target? "))
+    mark= False #bool(input("Do you want to mark the the next target? "))
     
-    firstcoord=int(input("Please give the number of the row you want to check: "))
+    firstcoord=1  #int(input("Please give the number of the row you want to check: "))
 
-    secondcoord=int(input("Please give the number of the column you want to check: "))
+    secondcoord=2  #int(input("Please give the number of the column you want to check: "))
 
 
-    if mine_list[firstcoord][secondcoord] == 10:
+
+    # gameover/check ellenőrzése
+    if table_list[firstcoord][secondcoord] == 10:
         if mark == True:
             minecounter -= 1
+            table_list[firstcoord][secondcoord] = 12
         elif mark == False:
             gameover = True
             print("Aknára léptél és felrobbantál a picsába.")
 
     minesaround = 0
 
+
+
+    # megnézi a megjelölt mező környzetében hány akna van
     if gameover == False and mark == False:
         if firstcoord == 0:
             for i in range(0,1):
                 if secondcoord == 0:
                     for j in range(0,1):
-                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                        if table_list[firstcoord+i][secondcoord+j] == 10 or table_list[firstcoord+i][secondcoord+j] == 12:
                             minesaround += 1
                 elif secondcoord == 8:
                     for j in range(-1,0):
-                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                        if table_list[firstcoord+i][secondcoord+j] == 10 or table_list[firstcoord+i][secondcoord+j] == 12:
                             minesaround += 1
                 else:
                     for j in range(-1,1):
-                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                        if table_list[firstcoord+i][secondcoord+j] == 10 or table_list[firstcoord+i][secondcoord+j] == 12:
                             minesaround += 1
         elif firstcoord == 8:
             for i in range(-1,0):
                 if secondcoord == 0:
                     for j in range(0,1):
-                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                        if table_list[firstcoord+i][secondcoord+j] == 10 or table_list[firstcoord+i][secondcoord+j] == 12:
                             minesaround += 1
                 elif secondcoord == 8:
                     for j in range(-1,0):
-                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                        if table_list[firstcoord+i][secondcoord+j] == 10 or table_list[firstcoord+i][secondcoord+j] == 12:
                             minesaround += 1
                 else:
                     for j in range(-1,1):
-                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                        if table_list[firstcoord+i][secondcoord+j] == 10 or table_list[firstcoord+i][secondcoord+j] == 12:
                             minesaround += 1
         else:
             for i in range(-1,1):
                 if secondcoord == 0:
                     for j in range(0,1):
-                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                        if table_list[firstcoord+i][secondcoord+j] == 10 or table_list[firstcoord+i][secondcoord+j] == 12:
                             minesaround += 1
                 elif secondcoord == 8:
                     for j in range(-1,0):
-                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                        if table_list[firstcoord+i][secondcoord+j] == 10 or table_list[firstcoord+i][secondcoord+j] == 12:
                             minesaround += 1
                 else:
                     for j in range(-1,1):
-                        if mine_list[firstcoord+i][secondcoord+j] == 10:
+                        if table_list[firstcoord+i][secondcoord+j] == 10 or table_list[firstcoord+i][secondcoord+j] == 12:
                             minesaround += 1
         table_list[firstcoord][secondcoord] = minesaround
         minesaround = 0
 
-
+    # table list kiprintelése
     for i in range(0, 9):
             for j in range(0, 9):
                 if table_list[i][j] == 0:
                     print("O", end=" ")
                 elif table_list[i][j] == 10:
-                    print("#", end=" ")
+                    print("O", end=" ")
                 elif table_list[i][j] == 9:
                     print("", end=" ")
+                elif table_list[i][j] == 11 or table_list[i][j] == 12:
+                    print("X", end=" ")
                 else:
                     print(table_list[i][j], end=' ')
             print("")
 
     print("")
 
+    # aknamező kiprintelése
+
     for i in range(0,9):
         for j in range(0,9):
-            print(mine_list[i][j], end= ' ')
+            print(table_list[i][j], end= ' ')
         print("")
 
 
