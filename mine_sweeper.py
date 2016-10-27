f8ab710cfc4f71622066d17349429398ec2b9647
Telakos <tel.akos.2@gmail.com>
2016-10-27 12:51:00 +0200

    testbuild updated

--- mine_sweeper.py
+++ mine_sweeper.py
@@ -1,73 +1,102 @@
-gameover = False
-
 import random
+
+gameover = False
 
 table_list= []
 
 mine_list=[]
 
+size = int(input("Please give us the minefield's size: "))
+
+minecounter = int(input("How many mines would you like to have on the minefield? "))
+
+if minecounter >= size*size:
+    gameover = true
+
+markcounter = minecounter + 1
+
 # 0-val táblázat feltöltés
-for i in range(0,9):
+for i in range(0,size+1):
     table_list.append([])
     mine_list.append([])
-    for j in range(0,9):
+    for j in range(0,size+1):
         table_list[i].append(0)
         mine_list[i].append(0)
 
 # aknák elhelyezése
-for i in range(0,15):
-    table_list[random.randint(0, 8)][random.randint(0, 8)] = 10
+for i in range(0,minecounter):
+    if table_list[random.randint(0, size)][random.randint(0, size)] == 0:
+        table_list[random.randint(0, size)][random.randint(0, size)] = 10
+    else:
+        i -= 1
 
-for i in range(0,9):
-    for j in range(0,9):
+for i in range(0,size+1):
+    for j in range(0,size+1):
         if table_list[i][j] == 10:
             mine_list[i][j] = "X"
 
 
 # table felprintelése
-for i in range(0, 9):
-    for j in range(0, 9):
+for i in range(0, size+1):
+    for j in range(0, size+1):
         print("O", end=" ")
     print("")
     
 
-    minecounter = 14
+
+
 
 while gameover == False:
 
-    
+    print("Remaining mines: ", minecounter, "Remaining marks: ", markcounter)
 
-    mark = "C" #input(Do you want to mark the the next target ('M' for mark and 'C' for check)? ")
-    mark = input("Do you want to mark the the next target ('M' for mark and 'C' for check)? ")
-
-    if mark == 'M':
-        mark = True
-    elif mark == 'C':
+    if markcounter > 0:
+        mark = input("Do you want to mark the the next target (M-mark, C-check, anything else-exit)? ")
+        if mark == 'M':
+            mark = True
+        elif mark == 'C':
+            mark = False
+        else:
+            gameover = True
+    else:
         mark = False
     
-    firstcoord = int(input("Please give the number of the row you want to check: "))
+    if gameover == False:
+        firstcoord = int(input("Please give the number of the row you want to check: "))
 
-    secondcoord = int(input("Please give the number of the column you want to check: "))
+        secondcoord = int(input("Please give the number of the column you want to check: "))
 
 
 
-    # gameover/check ellenőrzése
-    if table_list[firstcoord][secondcoord] == 10:
-        if mark == True:
-            minecounter -= 1
-            table_list[firstcoord][secondcoord] = 12
-        elif mark == False:
-            gameover = True
-            print("Aknára léptél és felrobbantál a picsába.")
-    elif table_list[firstcoord][secondcoord] == 12:
-        if mark == False:
-            gameover = True
-            print("Aknára léptél és felrobbantál a picsába.")
+    # gameover feltétel/mark ellenőrzése
+    if gameover == False:
+        if table_list[firstcoord][secondcoord] == 10:
+            if mark == True:
+                markcounter -= 1
+                minecounter -= 1
+                table_list[firstcoord][secondcoord] = 12
+            elif mark == False:
+                gameover = True
+                print("Aknára léptél és felrobbantál a picsába.")
+        elif table_list[firstcoord][secondcoord] == 12:
+            if mark == False:
+                gameover = True
+                print("Aknára léptél és felrobbantál a picsába.")
+            elif mark == True:
+                markcounter += 1
+        elif table_list[firstcoord][secondcoord] == 11:
+            if mark == True:
+                markcounter += 1
+
+
+    if mark == True and table_list[firstcoord][secondcoord] == 0:
+        markcounter -= 1
+        table_list[firstcoord][secondcoord] = 11
 
     minesaround = 0
 
 
-    while gameover == False and mark == False:
+    if gameover == False and mark == False:
         # megnézi a megjelölt mező környzetében hány akna van
         for i in range(-1,2):
             for j in range(-1,2):
@@ -86,26 +115,33 @@ while gameover == False:
 
 
     # table list kiprintelése
-    for i in range(0, 9):
-            for j in range(0, 9):
+    for i in range(0, size+1):
+        for j in range(0, size):
+            if i == 0 and j != 0:
+                print(j, end=" ")
+            elif i != 0 and j == 0:
+                print(i, end=" ")
+            elif i == 0 and j ==0:
+                print(" ",end=" ")
+            else:
                 if table_list[i][j] == 0:
                     print("O", end=" ")
                 elif table_list[i][j] == 10:
                     print("O", end=" ")
                 elif table_list[i][j] == 9:
-                    print("", end=" ")
+                    print(" ", end=" ")
                 elif table_list[i][j] == 11 or table_list[i][j] == 12:
                     print("X", end=" ")
                 else:
                     print(table_list[i][j], end=' ')
-            print("")
+        print("")
 
     print("")
 
     # aknamező kiprintelése
 
-    for i in range(0,9):
-        for j in range(0,9):
+    for i in range(0,size+1):
+        for j in range(0,size+1):
             print(table_list[i][j], end= ' ')
         print("")
     
